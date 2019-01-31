53d0c08b98f56136884ba35d7029239108c48022
GEORGIY NEMTSOV <gnemtsov@gmail.com>
2019-02-01 00:47:08 +0300

    Deploy v.2.0

---
+++ backend/frontend_build/precache-manifest.c9623336cf45c373b01bedd5546e144f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1af27bddebc3de472685",
+    "url": "/static/js/main.1af27bdd.chunk.js"
+  },
+  {
+    "revision": "1aa2b103a8a59cfd3855",
+    "url": "/static/js/1.1aa2b103.chunk.js"
+  },
+  {
+    "revision": "1af27bddebc3de472685",
+    "url": "/static/css/main.fa6f4d90.chunk.css"
+  },
+  {
+    "revision": "4c53724d2a4d27ae016948cf84edaa5e",
+    "url": "/index.html"
+  }
+];
